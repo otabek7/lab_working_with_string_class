93f261d2c05131f0c044fa2edb0e5f97f269a47a
Otabek Mavlonov <mavlonovo@lenovolaptop.localdomain>
2022-09-27 09:35:35 -0500

    filter first commit

---
+++ filter.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+using std::cin, std::cout, std::endl, std::string;
+
+int main() {
+   string sentence;
+   string details;
+
+   cout << "Please enter obfuscated sentence: ";
+   cin >> sentence;
+
+   cout << "Please enter deobfuscation details: ";
+   cin >> details;
+   
+   int i = 0;
+   int index = 0;
+   int temp = 0;
+
+   while(temp < sentence.length() && i < details.length()){
+    temp += details.at(i)-48;
+    cout << sentence.substr(index, temp) << " ";
+    i++;
+    index = temp;
+   }
+   
+
+
+    return 0;
+}
+
